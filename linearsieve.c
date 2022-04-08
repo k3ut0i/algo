eafd39505656c3a66c486e85bc239587f8e9ec70
Rajashekar R M <k3tu0isui@gmail.com>
2022-04-08 07:33:07 +0530

    linear sieve adapted to give smallest prime factors of all numbers

--- linearsieve.c
+++ linearsieve.c
@@ -36,6 +36,31 @@ int linearsieve(int n, int** primes){ // max:in, *primes:out, nprimes:ret
   return count;
 }
 
+static inline int next1(int* S, int i , int n){ // find a prime index after i
+  for(int j = i+1; j < n; j++)
+    if(S[j] == j) return j;
+  return -1; // Should never happen in linearsieve
+}
+
+int* smallest_prime_factor_ls(int n){ // adapting linear seive to get a prime factor
+  int* S = malloc(sizeof(int)*(n+1));
+  for(int i = 0; i <= n; i++) S[i] = i;
+  int p = 2, pi = 2;
+  while(p*p <= n){
+    int q = p, qi = pi;
+    while(p*q <= n){
+      int x = p*q;
+      while(x <= n)
+	S[x] = p, x = p*x;
+      qi = next1(S, qi, n-1);
+      q = S[qi];
+    }
+    pi = next1(S, pi, n-1);
+    p = S[pi];
+  }
+  return S;
+}
+
 int brute_sieve(int max, int** primes){ // max:in, *primes:out, nprimes:ret
   int default_size = max;
   int* primes_list = malloc(sizeof(int)*default_size);
@@ -62,25 +87,37 @@ int test_linear(int max){
   return 0;
 }
 
+int test_prime_factor(int max){
+  int* pf = smallest_prime_factor_ls(max);
+  for(int i = 0; i <= max; i++) fprintf(stdout, "%d\n", pf[i]);
+  return 0;
+}
+
 int main(int argc, char* argv[]){
-  if(argc!=3){
+  if(argc == 1){
     fprintf(stderr, "Usage: %s <type> <max_num>\n"
 	    "type           0: brute force, 1: linear sieve\n"
 	    , argv[0]);
     return -1;
+  } else if(argc == 2){
+    int num = atoi(argv[1]);
+    test_linear(num);
+    test_prime_factor(num);
+    return 0;
+  } else {
+    int type = atoi(argv[1]);
+    int num = atoi(argv[2]);
+    int** primes;
+    primes = malloc(sizeof(*primes));
+    int nprimes = 0;
+    if (type) nprimes = linearsieve(num, primes);
+    else nprimes = brute_sieve(num, primes);
+    fprintf(stdout, "Number of primes below %d is %d.\n", num, nprimes);
+    for(int i = 0; i < nprimes; i++)
+      fprintf(stdout, "%d\n", (*primes)[i]);
+    free(*primes);
+    free(primes);
+    /* test_linear(num); */
+    return 0;
   }
-  int type = atoi(argv[1]);
-  int num = atoi(argv[2]);
-  int** primes;
-  primes = malloc(sizeof(*primes));
-  int nprimes = 0;
-  if (type) nprimes = linearsieve(num, primes);
-  else nprimes = brute_sieve(num, primes);
-  fprintf(stdout, "Number of primes below %d is %d.\n", num, nprimes);
-  for(int i = 0; i < nprimes; i++)
-    fprintf(stdout, "%d\n", (*primes)[i]);
-  free(*primes);
-  free(primes);
-  /* test_linear(num); */
-  return 0;
 }
